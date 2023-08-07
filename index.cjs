e393499af09dbbc4fca6c71aa19f64c2c4a31fb6
lisonge <i@songe.li>
2023-08-07 20:47:58 +0800

    feat: init

---
+++ index.cjs
@@ -0,0 +1,1 @@
+module.exports = require('./kotlin/gkd-selector.js').li.songe.selector;
---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import GkdSelector from './kotlin/gkd-selector';
+declare const SelectorKit: typeof GkdSelector.li.songe.selector;
+
+export default SelectorKit;
